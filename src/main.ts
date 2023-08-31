1ef68f35200b9687866b7aceddc4a1dcedcbd992
Gaston <cutkillerone@gmail.com>
2023-08-30 22:09:04 -0400

    Cargando repo.
    Es la carga inicial con del back-end, tiene swaggert instalado.

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Rincon del Ton')
+    .setDescription('The Back-End web RDT')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000);
+}
+bootstrap();
